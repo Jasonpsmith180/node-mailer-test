0f7cc3a38e581e9bd8b65c2bcd41cb2fc0a54dcb
Jason <jasonpsmith180@gmail.com>
2021-09-13 21:05:47 -0600

    add post route

--- server.js
+++ server.js
@@ -9,11 +9,56 @@ const PORT = process.env.PORT || 5000;
 // initialize express app
 const app = express();
 
-// make contact the first page on the app
-app.route("/").get(function(req, res) {
-    res.sendfile(process.cwd() + "/public/index.html");
+// create transporter object
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASS
+    },
 });
 
+// verify connection config
+transporter.verify(function (error, success) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log("Server is ready to take our messages");
+    }
+});
+
+app.post("/send", (req, res) => {
+    let form = new multiparty.Form();
+    let data = {};
+    form.parse(req, function (err, fields) {
+        console.log(fields);
+        Object.keys(fields).forEach(function (property) {
+            data[property] = fields[property].toString();
+        });
+
+        const mail = {
+            from: data.name,
+            to: process.env.EMAIL,
+            subject: data.subject,
+            text: `${data.name} <${data.email}> \n${data.message}`,
+        };
+
+        transporter.sendMail(mail, (err, data) => {
+            if (err) {
+                console.log(err);
+                res.status(500).send("Something went wrong!");
+            } else {
+                res.status(200).send("Email successfully sent!")
+            }
+        });
+    });
+});
+
+// make contact the first page on the app
+app.route("/").get(function(req, res) {
+    res.send(process.cwd() + "/public/index.html");
+});
 
 // express server listening
 app.listen(PORT, () => {
